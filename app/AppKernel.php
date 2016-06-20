6aa1efcfdc55501f220a542ffc252385c005ec84
hatim serrakh <htm.serrakh@gmail.com>
2016-06-20 20:35:57 +0200

    creation de la base de donnée

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Backend\BackendBundle\BackendBackendBundle(),
+            new Frontend\FrontendBundle\FrontendFrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {

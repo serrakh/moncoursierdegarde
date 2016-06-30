df57c6be26038e898dca3c649b336d7b5ecacf2d
Mssef <youssef.programmeur@gmail.com>
2016-06-30 19:37:03 +0000

    install fos bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new Backend\BackendBundle\BackendBackendBundle(),
             new Frontend\FrontendBundle\FrontendFrontendBundle(),
         );

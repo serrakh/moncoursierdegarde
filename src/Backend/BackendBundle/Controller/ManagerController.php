6aa5deedb77dc242845953659e62bbf2dd55214c
Mssef <youssef.programmeur@gmail.com>
2016-07-02 21:11:42 +0000

    move the security code from app/config/security.yml to backendBundle/security.yml and create folder contain certain root of  fos bundle named rooting such as registration form login security and redirecting root...etc

---
+++ src/Backend/BackendBundle/Controller/ManagerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Backend\BackendBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\UserBundle\Model\UserInterface;
+
+class ManagerController extends Controller
+{
+    public function registerAction()
+    {
+        die;
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = 'frontend_homepage';
+            }
+
+            $this->addFlash(
+                'userep',
+                'votre compte à éte bien céer!'
+            );
+            $url = $this->container->get('router')->generate($route);
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+        return $this->render('BackendBackendBundle:UserManager:registration.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+}

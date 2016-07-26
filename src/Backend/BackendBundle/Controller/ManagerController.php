88025889820cb8d549189155161240a4881c17fb
hatim serrakh <htm.serrakh@gmail.com>
2016-07-26 13:42:33 +0200

    add form type of command

--- src/Backend/BackendBundle/Controller/ManagerController.php
+++ src/Backend/BackendBundle/Controller/ManagerController.php
@@ -4,14 +4,29 @@ namespace Backend\BackendBundle\Controller;
 
 use Backend\BackendBundle\Entity\Client;
 use Backend\BackendBundle\Form\ClientType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Backend\BackendBundle\Form\CommandeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use FOS\UserBundle\Model\UserInterface;
 
 class ManagerController extends Controller
 {
+    public function commandeAction(Request $request){
+        $em = $this->getDoctrine()->getEntityManager();
+        $client = $em->getRepository('BackendBackendBundle:Client');
+        $c0 = $client->find(1);
+        $form = $this->createForm(new CommandeType());
+        $form->handleRequest($request);
+        if ($request->isMethod('post')) {
+            if ($form->isValid()) {
+                return new Response('ok');
+            }
+        }
+        return $this->render('@BackendBackend/command.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
     public function registerAction(Request $request)
     {
         $em = $this->getDoctrine()->getEntityManager();
